7236425870e2226ab8d887f33bc4db41042a7f4a
swarvanusg <swarvanusg@gmail.com>
2015-10-05 20:46:10 +0530

    Added Examples, Fixed issues related to method discovery

---
+++ Example/IWorkUsingPlugin.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	GoPlug "github.com/swarvanusg/GoPlug"
+	"os"
+)
+
+func main() {
+	plugRegConf := GoPlug.PluginRegConf{PluginLocation: "Plugin", AutoDiscover: true, ConfExt: ".pconf"}
+	pluginReg, regErr := GoPlug.PluginRegInit(plugRegConf)
+	if regErr != nil {
+		fmt.Printf("Plugin reg init failed")
+		return
+	}
+
+	/* Wait for input */
+	fmt.Print("Press 'Enter' to continue...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+
+	/* Check if plug1 is loaded */
+	/*
+		if pluginReg.IsLoaded("Do", "Test") {
+			fmt.Printf("plug1 is loaded")
+			return
+		}*/
+
+	/* Load plug1, as lazy load is configured */
+	/*
+		plug1, err := pluginReg.LoadPlugin("Plug1", "Monitor")
+		if err != nil {
+			fmt.Printf("Plugin loading failed : %s\n", err)
+			return
+
+		}*/
+
+	/* get a plugin */
+	plugin1 := pluginReg.GetPlugin("Do", "Test")
+	if plugin1 == nil {
+		fmt.Printf("Get a plugin failed")
+		return
+	}
+
+	methods := plugin1.GetMethods()
+	/* get the list of method */
+	fmt.Println("Methods: ", methods, "\n")
+	for _, value := range methods {
+		plugin1.Execute(value, nil)
+	}
+
+	/* Wait for input */
+	fmt.Print("Press 'Enter' to continue...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+
+	/* unload the Plugin */
+	unloadErr := pluginReg.UnloadPlugin(plugin1)
+	if unloadErr != nil {
+		fmt.Printf("Unload of a plugin failed")
+		return
+	}
+
+	pluginReg.Stop()
+	pluginReg.WaitForStop()
+}
---
+++ Example/Plugin/IAmPlugin.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"com.ss/GoPlugin_test/GoPlug"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+
+	config := GoPlug.PluginImplConf{"./", "Do", "Test", "", false, activate, stop}
+	plugin, err := GoPlug.PluginInit(config)
+	if err != nil {
+		fmt.Printf("Plugin Init Error: %s\n", err)
+		return
+	}
+	plugin.RegisterMethod("Do", Do)
+	plugin.Start()
+	shutdownChannel := makeShutdownChannel()
+
+	//we block on this channel
+	<-shutdownChannel
+	plugin.Stop()
+}
+
+func makeShutdownChannel() chan os.Signal {
+	//channel for catching signals of interest
+	signalCatchingChannel := make(chan os.Signal)
+
+	//catch Ctrl-C and Kill -9 signals
+	signal.Notify(signalCatchingChannel, syscall.SIGINT, syscall.SIGTERM)
+
+	return signalCatchingChannel
+}
+
+func activate(data []byte) []byte {
+	fmt.Printf("Activating Plugin\n")
+	return nil
+}
+
+func stop(data []byte) []byte {
+	fmt.Printf("Stoping Plugin\n")
+	return nil
+}
+
+func Do(data []byte) []byte {
+	fmt.Printf("I'm Doing\n")
+	return nil
+}
